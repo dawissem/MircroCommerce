8872d987f3e5d824d90dc7238a6181926172d172
dawissem <dawissem.wm@gmail.com>
2024-06-26 14:07:26 +0100

    project during session of formation withtachmycode //website -commerce servicer cruud product done

---
+++ product-service/src/test/java/com/ecommerceMicro/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,21 @@
+package com.ecommerceMicro.product_service;
+
+import com.ecommerceMicro.product_service.dto.ProductRequest;
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
+
+import java.math.BigDecimal;
+
+
+class ProductServiceApplicationTests {
+
+
+
+}
