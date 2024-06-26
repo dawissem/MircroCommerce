7d0c5de67aff21cd706c0e9e5f97b4ee80d2b3ea
dawissem <dawissem.wm@gmail.com>
2024-06-26 23:35:04 +0100

    implemenation test ajout produit

--- product-service/src/test/java/com/ecommerceMicro/product_service/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/ecommerceMicro/product_service/ProductServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package com.ecommerceMicro.product_service;
 
 import com.ecommerceMicro.product_service.dto.ProductRequest;
+import com.ecommerceMicro.product_service.model.Product;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,9 +14,56 @@ import org.testcontainers.containers.MongoDBContainer;
 
 import java.math.BigDecimal;
 
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+        //classe thika pour tester els methodes
 class ProductServiceApplicationTests {
+
+   @ServiceConnection
+
+
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.14-rc0-focal");
+
+
+   @LocalServerPort
+    private Integer port;
+
+   @BeforeEach
+    void setUp() {
+       RestAssured.baseURI ="http://localhost";
+       RestAssured.port = port ;
+
+   }
+    static {
+       mongoDBContainer.start();
+    }
+
+
+    @Test
+    void shouldCreateProduct() throws Exception {
+        ProductRequest productRequest = getProductRequest();
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(productRequest)
+                .when()
+                .post("/api/product")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("id",Matchers.notNullValue())
+                .body("name",Matchers.equalTo(productRequest.name()))
+                .body("description",Matchers.equalTo(productRequest.description()))
+                .body("price",Matchers.is(productRequest.price().intValueExact()))
+                .body("categoryId",Matchers.equalTo(productRequest.categoryId()));
+    }
+
+
+    private ProductRequest getProductRequest() {
+       return new ProductRequest("null","iphone13", "last model of name",BigDecimal.valueOf(1200),"1");
+    }
 
 
 
 }
+
+
